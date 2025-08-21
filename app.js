8fefd53535fe3689bacd578296d01d075205ee0e
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-21 14:58:13 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const authorization =
   "btPvqjuzJrQ8pCaDuu1OPYNnDqUQlepnJo86ma3BzeBDy1GzEWJe28rc";
 
+let fetchLink = "";
+
 async function fetchApi() {
   const dataFetch = await fetch(
     "https://api.pexels.com/v1/curated?per_page=15&page=1",
@@ -12,6 +14,14 @@ async function fetchApi() {
       },
     }
   );
-  console.log(dataFetch);
+  const data = await dataFetch.json();
+  return data;
 }
-fetchApi();
+// fetchApi();
+
+async function curatedPhotos() {
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
+  console.log(data.photos);
+}
+curatedPhotos();
