3e6c427a2a7e131ca481dd5c499b93f790695f42
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-22 10:40:18 +0530

    implement picture generation from API response

--- app.js
+++ app.js
@@ -19,9 +19,25 @@ async function fetchApi() {
 }
 // fetchApi();
 
+const generatePictures = (data) => {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+  
+  <div class="gallery-info">
+  <p>${photo.photographer}</p>
+  <a target="_blank" href="${photo.src.original}"></a>
+  </div>
+  <img src="${photo.src.original}"/>`;
+    gallery.appendChild(galleryImg);
+  });
+};
+
 async function curatedPhotos() {
   fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
   const data = await fetchApi(fetchLink);
-  console.log(data.photos);
+  // console.log(data.photos);
+  generatePictures(data);
 }
 curatedPhotos();
